0443182edc5690a901536218cddb11e4e9ccdcd4
Eric Barr <eb@eb2.co>
2024-12-02 02:51:16 -0700

    Moved tolerance safety check to function

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,11 +1,28 @@
 advent_of_code::solution!(2);
 
-pub fn is_safe(input: &[i32]) -> bool {
+fn is_safe(input: &[i32]) -> bool {
     let diffs: Vec<i32> = input.windows(2).map(|x| x[0] - x[1]).collect();
     let all_positive_or_negative = diffs.iter().all(|&x| x > 0) || diffs.iter().all(|&x| x < 0);
     let within_bounds = diffs.iter().all(|&x| x.abs() > 0 && x.abs() < 4);
 
     all_positive_or_negative && within_bounds
+}
+
+fn is_safe_with_tolerance(input: &[i32]) -> bool {
+    if is_safe(&input) {
+        return true;
+    }
+
+    for i in 0..input.len() {
+        let mut temp_nums = input.to_vec();
+        temp_nums.remove(i);
+
+        if is_safe(&temp_nums) {
+            return true;
+        }
+    }
+
+    false
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -33,20 +50,7 @@ pub fn part_two(input: &str) -> Option<u32> {
                 .filter_map(|x| x.parse::<i32>().ok())
                 .collect();
 
-            if is_safe(&nums) {
-                return true;
-            }
-
-            for i in 0..nums.len() {
-                let mut temp_nums = nums.clone();
-                temp_nums.remove(i);
-
-                if is_safe(&temp_nums) {
-                    return true;
-                }
-            }
-
-            false
+            is_safe_with_tolerance(&nums)
         })
         .count();
 
